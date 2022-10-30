5ac0e480ea04e821afcc2e397d2f6e6bef1423de
jiten2318 <117006357+jiten2318@users.noreply.github.com>
2022-10-30 14:47:10 +0530

    Added List Programs

---
+++ list5.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 30 14:40:15 2022
+
+@author: DELL
+"""
+
+# Python program to find smallest
+# number in a list
+
+# list of numbers
+list1 = [10, 20, 4, 45, 99]
+
+# sorting the list
+list1.sort()
+
+# printing the first element
+print("Smallest element is:", list1[0])
